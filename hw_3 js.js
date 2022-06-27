f3e699732efb4811983ae7760564641fd1231acb
vladkashokoladka <vlada.malanchuk93@gmail.com>
2022-06-27 13:16:28 +0300

    main

--- hw_3 js.js
+++ hw_3 js.js
@@ -1,31 +1,97 @@
-let dat1 = '09.10.2021';
-let dat2 = '10.10.2021';
 
-var user0 = new Map();
-user0.set('firstName', 'John');
-user0.set('lastName', 'Caper');
-user0.set('phone', '7436676737634');
-user0.set('registrationDate', '12.10.2021');
-
-var user1 = new Map();
-user1.set('firstName', 'Clark');
-user1.set('lastName', 'Kent');
-user1.set('phone', '4346676737634');
-user1.set('registrationDate', '10.10.2021');
-
-var users = [user0, user1];
-
-function findDateOfReg(users) {
-   
-   for (let i = 0; i < users.length; i++){
-      
-         if(dat2==users[i].get('registrationDate') || dat1==users[i].get('registrationDate'))
-            console.log(users[i])
-     
-   }
-
-   return ;
+const users = [
+   {
+       firstName: 'John',
+       lastName: 'Caper',
+       phone: '7436676737634',
+       registrationDate: '12.10.2021'
+   },
+   {
+       firstName: 'Clark',
+       lastName: 'Kent',
+       phone: '4346676737634',
+       registrationDate: '16.09.2021'
+   },
+   {
+       firstName: 'Tony',
+       lastName: 'Stark',
+       phone: '7436698337634',
+       registrationDate: '11.10.2021'
+   },
+   {
+       firstName: 'Bruce',
+       lastName: 'Wayne',
+       phone: '1111176737634',
+       registrationDate: '09.10.2021'
+   },
+   {
+       firstName: 'Star',
+       lastName: 'Lord',
+       phone: '7439374737634',
+       registrationDate: '10.10.2021'
+   },
+   {
+       firstName: 'Kate',
+       lastName: 'Bishop',
+       phone: '7436693647634',
+       registrationDate: '11.10.2021'
+   },
+   {
+       firstName: 'Jerry',
+       lastName: 'James',
+       phone: '7409048737634',
+       registrationDate: '10.10.2021'
+   },
+   {
+       firstName: 'Jeremy',
+       lastName: 'Clarkson',
+       phone: '743667600289334',
+       registrationDate: '16.10.2020'
+   },
+   {
+       firstName: 'Robert',
+       lastName: 'Patrik',
+       phone: '7436676730093',
+       registrationDate: '10.10.2020'
+   },
+   {
+       firstName: 'Jonny',
+       lastName: 'Sins',
+       phone: '74923982737634',
+       registrationDate: '01.01.2021'
+   },
+   {
+       firstName: 'Andrew',
+       lastName: 'Garfield',
+       phone: '743667988344',
+       registrationDate: '09.10.2019'
+   },
+   {
+       firstName: 'Jane',
+       lastName: 'Foster',
+       phone: '74368783427634',
+       registrationDate: '09.10.2019'
+   },
+   {
+       firstName: 'Rick',
+       lastName: 'Smith',
+       phone: '700000037634',
+       registrationDate: '12.10.2021'
+   },
+];
+const printUsersByRegDates = function (users, regDates) {
+   users.forEach(user => {
+       if (regDates.includes(user.registrationDate)) console.log(user);
+   });
 }
 
-findDateOfReg(users);
+printUsersByRegDates(users, ['09.10.2021', '10.10.2021']);
 
+
+
+
+// const filterUsersByRegDates = function(users, regDates){
+//    return users.filter(element => regDates.includes(element.registrationDate))
+// }
+
+// console.log(filterUsersByRegDates(users, ['09.10.2021', '10.10.2021']));
